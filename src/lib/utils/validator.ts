fe131e874324a7f98b0680f117dd1b3344eac1b5
sangyeon kim <greyhairchooselife@gmail.com>
2023-02-23 15:25:27 +0900

    fix: 타입 지정하지 않은 오류 수정

--- src/lib/utils/validator.ts
+++ src/lib/utils/validator.ts
@@ -1,9 +1,9 @@
-export const isEmailValide = (email) => {
-    if (email.split('').includes('@')) return true;
-    else return false;
+export const isEmailValide = (email: string) => {
+  if (email.split('').includes('@')) return true;
+  else return false;
 };
 
-export const isPswValide = (psw) => {
-    if (psw.length >= 8) return true;
-    else return false;
+export const isPswValide = (psw: string) => {
+  if (psw.length >= 8) return true;
+  else return false;
 };
