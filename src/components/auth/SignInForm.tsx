42d8a2700e652a16808afbeda530c545c12c99e7
tnghgks <tnghgks@naver.com>
2023-02-23 14:23:40 +0900

    feat: 로그인,회원가입 기능 추가

--- src/components/auth/SignInForm.tsx
+++ src/components/auth/SignInForm.tsx
@@ -1,18 +1,41 @@
-import { useMemo } from 'react';
-import { Link } from 'react-router-dom';
+import { useCallback, useMemo, useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import useInput from '@/lib/hooks/common/useInput';
 import { validator } from '@/lib/utils/validator';
+import authApi from '@/api/auth/auth';
 
 const SignInForm = () => {
   const [email, isValidEmail] = useInput('', validator.email);
   const [password, isValidPw] = useInput('', validator.password);
+  const [error, setError] = useState('');
+  const navigate = useNavigate();
   const isDisabled = useMemo(
     () => ![isValidEmail.value, isValidPw.value].every((valid) => valid),
     [isValidEmail.value, isValidPw.value],
   );
 
+  const handleSubmit = useCallback(
+    async (e: React.FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+      setError('');
+
+      const res = await authApi.signIn({
+        email: email.value,
+        password: password.value,
+      });
+
+      if (res?.data.message) return setError(res.data.message);
+
+      if (res?.status === 200) {
+        localStorage.setItem('token', res.data.access_token);
+        return navigate('/todo');
+      }
+    },
+    [email.value, password.value, navigate],
+  );
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <label htmlFor="email">이메일</label>
       <input id="email" type="text" data-testid="email-input" {...email} />
       <p>{isValidEmail.message}</p>
@@ -24,6 +47,7 @@ const SignInForm = () => {
         {...password}
       />
       <p>{isValidPw.message}</p>
+      <p>{error}</p>
       <button data-testid="signin-button" disabled={isDisabled}>
         로그인
       </button>
--- src/components/auth/SignUpForm.tsx
+++ src/components/auth/SignUpForm.tsx
@@ -1,18 +1,37 @@
+import authApi from '@/api/auth/auth';
 import useInput from '@/lib/hooks/common/useInput';
 import { validator } from '@/lib/utils/validator';
-import { useMemo } from 'react';
-import { Link } from 'react-router-dom';
+import { useCallback, useMemo, useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 
 const SignUpForm = () => {
   const [email, isValidEmail] = useInput('', validator.email);
   const [password, isValidPw] = useInput('', validator.password);
+  const [error, setError] = useState('');
+  const navigate = useNavigate();
   const isDisabled = useMemo(
     () => ![isValidEmail.value, isValidPw.value].every((valid) => valid),
     [isValidEmail.value, isValidPw.value],
   );
 
+  const handleSubmit = useCallback(
+    async (e: React.FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+
+      const res = await authApi.signUp({
+        email: email.value,
+        password: password.value,
+      });
+
+      if (res?.data.message) return setError(res.data.message);
+
+      if (res?.status === 201) return navigate('/signin');
+    },
+    [email.value, password.value, navigate],
+  );
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <label htmlFor="email">이메일</label>
       <input id="email" type="text" data-testid="email-input" {...email} />
       <p>{isValidEmail.message}</p>
@@ -24,6 +43,7 @@ const SignUpForm = () => {
         {...password}
       />
       <p>{isValidPw.message}</p>
+      <p>{error}</p>
       <button data-testid="signup-button" disabled={isDisabled}>
         회원가입
       </button>
