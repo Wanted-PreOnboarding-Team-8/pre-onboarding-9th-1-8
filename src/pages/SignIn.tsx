89da5f03deab7bdcfd425f5f8b927560b6c6f743
tnghgks <tnghgks@naver.com>
2023-02-23 14:31:02 +0900

    feat: Token 보유 시 Todos로 리다이렉트

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,6 +1,17 @@
 import SignInForm from '@/components/auth/SignInForm';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const SignIn = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      navigate('/todo');
+    }
+  }, [navigate]);
+
   return (
     <main>
       <SignInForm />
--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,6 +1,17 @@
 import SignUpForm from '@/components/auth/SignUpForm';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const SignUp = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      navigate('/todo');
+    }
+  }, [navigate]);
+
   return (
     <main>
       <SignUpForm />
